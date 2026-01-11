e73c395ee3501644e11b52aebfd782c8d933efba
jack <jack@chinaphp.com>
2026-01-12 00:52:04 +0800

    fix: 更新容器证书路径和pause镜像版本匹配逻辑

--- auto-build.sh
+++ auto-build.sh
@@ -143,7 +143,7 @@ sudo sed -i "s/registry.k8s.io/sea.hub:5000/g" rootfs/etc/kubeadm.yml.tmpl
 pauseImage=$(./"${ARCH}"/bin/kubeadm config images list --config "rootfs/etc/kubeadm.yml" 2>/dev/null | sed "/WARNING/d" | grep pause)
 
 echo "pauseImage: $pauseImage"
-if [ -f "rootfs/etc/dump-config.toml" ]; then sudo sed -i "s/sea.hub:5000\/pause:3.6/$(echo "$pauseImage" | sed 's/\//\\\//g')/g" rootfs/etc/dump-config.toml; fi
+if [ -f "rootfs/etc/dump-config.toml" ]; then sudo sed -i "s/sea.hub:5000\/pause:[^\"]*/$(echo "$pauseImage" | sed 's/\//\\\//g')/g" rootfs/etc/dump-config.toml; fi
 
 # fix
 if [ -f "rootfs/kubeadm.yaml" ];then
